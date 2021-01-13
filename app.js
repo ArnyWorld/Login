3074fbd8eb5d61a8f1ed488acf44491cb58f3201
Arnaldo Muñoz Mendoza <47994871+ArnyWorld@users.noreply.github.com>
2021-01-13 01:31:31 -0400

    Mejoras JS

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ let loginform = document.getElementById('loginform');
 let regform = document.getElementById('regform');
 let btndesign = document.getElementById('btndesign');
 
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+
 function logform() {
 
     if (window.matchMedia('(max-width:768px)').matches) {
         loginform.style.left = "-500px";
         regform.style.left = "11%";
         btndesign.style.left = "110px";
+         btn1.style.color = "black";
+        btn2.style.color = "white";
     }
 
     if (window.matchMedia('(max-width:812px)').matches) {
         loginform.style.left = "-500px";
         regform.style.left = "12%";
         btndesign.style.left = "110px";
+        btn1.style.color = "black";
+        btn2.style.color = "white";
     } else {
         loginform.style.left = "-500px";
         regform.style.left = "15%";
         btndesign.style.left = "110px";
+        btn1.style.color = "black";
+        btn2.style.color = "white";
     }
 
 
@@ -29,16 +38,22 @@ function rgform() {
         loginform.style.left = "11%";
         regform.style.left = "450px";
         btndesign.style.left = "0px";
+        btn2.style.color = "black";
+        btn1.style.color = "white";
     }
 
     if (window.matchMedia('(max-width:812px)').matches) {
         loginform.style.left = "12%";
         regform.style.left = "450px";
         btndesign.style.left = "0px";
+        btn2.style.color = "black";
+        btn1.style.color = "white";
     } else {
         loginform.style.left = "15%";
         regform.style.left = "450px";
         btndesign.style.left = "0px";
+        btn2.style.color = "black";
+        btn1.style.color = "white";
     }
 
 }
